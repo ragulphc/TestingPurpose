4a697224e9291d7f6747f345beca35f0100dc801
ragulphc <ragul.p@avasoft.com>
2022-10-23 04:26:16 +0530

    Initial commit

---
+++ MiddleWare/servicelayer.php
@@ -0,0 +1,42 @@
+<?php 
+ 
+ /**
+    * afsdasdf 
+    *
+    * @package          asdfdsafasdfasdfadshjfvhasdnf
+    * @author           adfsadsfasd 
+    * @copyright        fasf   
+    * @license          asdfasdfdasf 
+    * 
+    * @wordpress-plugin
+    * Plugin Name:      afsdasdf
+    * Plugin URI:       asdfadsf 
+    * Description:      adsf
+    * Version:          asdfasdf 
+    * Author:           asdfasdfadsf
+    * Author URI:       afdsadfs
+    * Text Domain:      afsdafsd 
+    * License:          asdf 
+    * License URI:      adsfasdfasdf 
+    */ 
+ 
+
+    /**
+     * Plugin Version
+    */
+    if(!defined('afsdasdf_version')){
+      define('nextcare_api_service_plugin_version', '1.0.0');
+    } 
+    
+
+    /**
+     * Plugin URL
+    */
+    if(!defined('afsdasdf_version')){
+      define('nextcare_api_service_plugin_dir', plugin_dir_path(__FILE__));
+    } 
+  
+    require_once afsdasdf_dir . 'EnvReader.php';
+    
+ 
+?>
